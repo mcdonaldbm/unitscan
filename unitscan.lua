d616e9e6383df890ba77a515b32b25eaf0b5ec3b
Bryce McDonald <mcdonaldbm@users.noreply.github.com>
2021-06-19 17:18:11 -0500

    Update unitscan.lua

--- unitscan.lua
+++ unitscan.lua
@@ -44,7 +44,7 @@ function unitscan.target(name)
 	if forbidden then
 		if not found[name] then
 			SendChatMessage('{skull} Detected ' .. name ..' in ' .. GetZoneText() ..' {skull}' , 'GUILD' , nil)
-            		C_Timer.After(5, function () SendChatMessage('{skull} Detected ' .. name ..' in ' .. GetZoneText() ..' {skull}' , 'GUILD' , nil) end)
+            		C_Timer.After(120, function () SendChatMessage('{skull} Detected ' .. name ..' in ' .. GetZoneText() ..' {skull}' , 'GUILD' , nil) end)
 			found[name] = true
 			FlashClientIcon()
 			unitscan.play_sound()
